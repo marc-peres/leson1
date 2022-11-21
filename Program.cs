92a79a49ab3ed614ecb11c4ef809238e0e4050b0
Marcos Peres <peres_m@mail.ru>
2022-11-21 20:54:19 +0300

    Init commit

--- Program.cs
+++ Program.cs
@@ -1,8 +1,62 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
-int number = int.Parse(Console.ReadLine());
+//int number = int.Parse(Console.ReadLine());
 
-int quarter = number * number;
+//int quarter = number * number;
 
-Console.WriteLine(quarter);
+//Console.WriteLine(quarter);
+
+//Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+//a = 25, b = 5 -> да a = 2, b = 10 -> нет a = 9, b = -3 -> да a = -3 b = 9 -> нет
+//20 мин
+
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+
+//if (a == b * b)
+//{
+//    Console.WriteLine("Да");
+//}
+//else
+// {
+//     Console.WriteLine("Нет");
+// }
+
+// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда 5 -> Пятница
+
+// int a = int.Parse(Console.ReadLine());
+
+// if (a == 1)
+// {
+//     Console.WriteLine("Понедельник");
+// }
+// else if (a == 2)
+// {
+//     Console.WriteLine("Вторник");
+// }
+// else if (a == 3)
+// {
+//     Console.WriteLine("Среда");
+// }
+// else if (a == 4)
+// {
+//     Console.WriteLine("Четверг");
+// }
+// else if (a == 5)
+// {
+//     Console.WriteLine("Пятница");
+// }
+// else if (a == 6)
+// {
+//     Console.WriteLine("Суббота");
+// }
+// else if (a == 7)
+// {
+//     Console.WriteLine("Воскресенье");
+// }
+// else
+// {
+//     Console.WriteLine("Ошибка");
+// }
