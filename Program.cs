7a0516ffd3a00901863de30d0cd7a2fb1dd60979
Marcos Peres <peres_m@mail.ru>
2022-11-21 20:31:29 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int number = int.Parse(Console.ReadLine());
+
+int quarter = number * number;
+
+Console.WriteLine(quarter);
