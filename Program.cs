8133fa7266e735abd7e5a02d28bda7c54cb8e67c
Marcos Peres <peres_m@mail.ru>
2022-11-24 17:19:56 +0300

    HomeWorke for leson1

--- Program.cs
+++ Program.cs
@@ -1,62 +1,60 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
 
-//int number = int.Parse(Console.ReadLine());
-
-//int quarter = number * number;
-
-//Console.WriteLine(quarter);
-
-//Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
-//a = 25, b = 5 -> да a = 2, b = 10 -> нет a = 9, b = -3 -> да a = -3 b = 9 -> нет
-//20 мин
-
-//int a = int.Parse(Console.ReadLine());
-//int b = int.Parse(Console.ReadLine());
-
-//if (a == b * b)
-//{
-//    Console.WriteLine("Да");
-//}
-//else
-// {
-//     Console.WriteLine("Нет");
-// }
-
-// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
-// 3 -> Среда 5 -> Пятница
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
 // int a = int.Parse(Console.ReadLine());
-
-// if (a == 1)
+// int b = int.Parse(Console.ReadLine());
+// if (a > b)
 // {
-//     Console.WriteLine("Понедельник");
+//     Console.WriteLine(a + " - больше");
 // }
-// else if (a == 2)
+// else if (a < b)
 // {
-//     Console.WriteLine("Вторник");
-// }
-// else if (a == 3)
-// {
-//     Console.WriteLine("Среда");
-// }
-// else if (a == 4)
-// {
-//     Console.WriteLine("Четверг");
-// }
-// else if (a == 5)
-// {
-//     Console.WriteLine("Пятница");
-// }
-// else if (a == 6)
-// {
-//     Console.WriteLine("Суббота");
-// }
-// else if (a == 7)
-// {
-//     Console.WriteLine("Воскресенье");
+//     Console.WriteLine(b + " - больше");
 // }
 // else
 // {
-//     Console.WriteLine("Ошибка");
+//     Console.WriteLine("Числа ровны");
+// }
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// int a = int.Parse(Console.ReadLine());
+// int b = int.Parse(Console.ReadLine());
+// int c = int.Parse(Console.ReadLine());
+// int max = a;
+// if (b > max)
+// {
+//     max = b;
+// }
+// if (c > max)
+// {
+//     max = c;
+// }
+// Console.WriteLine(max + " - максимальное число");
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// int a = int.Parse(Console.ReadLine());
+// if (a % 2 == 0)
+// {
+//     Console.WriteLine(a + " - четное");
+// }
+// else
+// {
+//     Console.WriteLine(a + " - нечетное");
+// }
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// int a = int.Parse(Console.ReadLine());
+// int i = 1;
+
+// while (i < a) // Не включая N
+// {
+//     if (i % 2 == 0)
+//     {
+//         Console.WriteLine(i);
+//     }
+//     i++;
 // }
